9e42fdbdc27fae84addfe5a550a8924a2df30602
Disfinder Root <3998461+disfinder@users.noreply.github.com>
2024-10-28 22:13:15 -0400

    replace constants with parameters

--- tatogenerator.py
+++ tatogenerator.py
@@ -2,16 +2,25 @@
 
 import time
 import random
+import argparse
 
-WORDS_MAIN = ['mama', 'tato', 'Max']
-WORDS_SUPPLEMENTAL = ['a', 'b', 'c', 'd', 'z']
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--words-main', action="append", default=['mama', 'tato', 'Max'])
+    parser.add_argument('--words-other', action="append", default=['a', 'b', 'c', 'd', 'z'])
+    return parser.parse_args()
+
+
 WORDS_COUNT = 100
+args = parse_args()
 
 random.seed(time.time())
 for i in range(WORDS_COUNT):
-    if random.randrange(100) <10:
-        list_for_next_word = WORDS_MAIN
+    if random.randrange(100) < 10:
+        list_for_next_word = args.words_main
     else:
-        list_for_next_word = WORDS_SUPPLEMENTAL
+        list_for_next_word = args.words_other
     next_word = random.choice(list_for_next_word)
     print(next_word, end=' ')
