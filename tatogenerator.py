4ba7aa97f95b6507f925bfe250e1182a7d345e40
Disfinder Root <3998461+disfinder@users.noreply.github.com>
2024-11-04 11:21:26 -0500

    transform WORDS_COUNT constant into parameter

--- tatogenerator.py
+++ tatogenerator.py
@@ -10,14 +10,14 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--words-main', action="append", default=['mama', 'tato', 'Max'])
     parser.add_argument('--words-other', action="append", default=['a', 'b', 'c', 'd', 'z'])
+    parser.add_argument('--words-count', type=int, default=100)
     return parser.parse_args()
 
 
-WORDS_COUNT = 100
 args = parse_args()
 
 random.seed(time.time())
-for i in range(WORDS_COUNT):
+for i in range(args.words_count):
     if random.randrange(100) < 10:
         list_for_next_word = args.words_main
     else:
